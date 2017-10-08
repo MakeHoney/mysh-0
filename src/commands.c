63be95c2a5c02f2fe78d48c0ce222df98a860a11
MakeHoney <buffalo1332@gmail.com>
2017-10-08 23:53:14 +0900

    maybe final ver

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,49 @@
+#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
 
-  // TODO: Fill it!
-  return 0;
+	if (!validate_cd_argv(argc, argv) || chdir(argv[1]))
+ 	   return -1;
+	  return 0;
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
 
-  // TODO: Fill it!
+	char buf[255];
+	if (!validate_pwd_argv(argc, argv))
+    	return -1;
 
-  return 0;
+	getcwd(buf, 255);
+	printf("%s\n", buf);
+
+	return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	int dotCount = 0;
+
+	for(int i = 0 ; i < argc ; i++)
+		if(!strcmp(argv[i], "."))
+			dotCount++;
+
+	if(argc >= 2) {
+		if(strcmp(argv[0], "cd"))
+			return 0;
+	}
+
+	if(dotCount > 1 || argc == 1)
+		return 0;
+
+	return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if(!strcmp(argv[0], "pwd") && argc == 1)
+		return 1;
+
+	return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,41 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "utils.h"
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
-{
-  // TODO: Fill this!
+void mysh_parse_command(const char* command, 
+						int* argc, char*** argv) {
+
+	char tempBuf[50];
+	int blankFlag = 0;
+	int tempIdx = 0;
+	int SIZEOFSTRING = sizeof(char);
+	*argc = 0;
+
+	argv[0] = (char**)malloc(sizeof(char*));
+	
+	for (int i = 0; command[i] != '\0'; i++) {
+		if(command[i] == ' ' || command[i] == '\n' || command[i] == '\t')
+			continue;
+
+		blankFlag++;
+		SIZEOFSTRING += sizeof(char);
+
+		tempBuf[tempIdx++] = command[i];
+
+		if (command[i + 1] == ' ' || command[i + 1] == '\t' ||
+				command[i + 1] == '\n' || command[i + 1] == '\0') {
+			tempBuf[tempIdx] = '\0';
+			argv[0][*argc] = (char*)malloc(SIZEOFSTRING);
+			strcpy(argv[0][(*argc)++], tempBuf);
+			SIZEOFSTRING = sizeof(char);
+			tempIdx = 0;
+		}
+	}
+
+	if(blankFlag == 0){
+		argv[0][0] = (char*)malloc(sizeof(char) * 50);
+		strcpy(argv[0][0], "");
+		(*argc)++;
+	}
 }
